b9f35c3c3125f57cc0b948c17e895b6608734f42
Bowen <huangbowen521@gmail.com>
2014-02-21 23:42:20 +0800

    refactor

--- src/main/java/huangbowen/net/EmbedBrokerApp.java
+++ src/main/java/huangbowen/net/EmbedBrokerApp.java
@@ -1,34 +1,12 @@
 package huangbowen.net;
 
-import huangbowen.net.jms.MessageSender;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.util.StringUtils;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
+import static huangbowen.net.SenderApp.start;
 
 public class EmbedBrokerApp
 {
     public static void main( String[] args ) throws IOException {
         start("embedBroker.xml");
-    }
-
-    public static void start(String configLocation) throws IOException {
-        MessageSender sender = getMessageSender(configLocation);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String text = br.readLine();
-
-        while (!StringUtils.isEmpty(text)) {
-            System.out.println(String.format("send message: %s", text));
-            sender.send(text);
-            text = br.readLine();
-        }
-    }
-
-    public static MessageSender getMessageSender(String configLocation) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(configLocation);
-       return (MessageSender) context.getBean("messageSender");
     }
 }
